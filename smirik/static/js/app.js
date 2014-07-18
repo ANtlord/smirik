253ea087abb160dc86f8ee9e2fb14e923b1eaf83
antlord <antlord92@gmail.com>
2014-07-18 11:47:51 +0900

    list of stocks was created on account view

--- smirik/static/js/app.js
+++ smirik/static/js/app.js
@@ -1,1 +1,1 @@
-var app = angular.module('App', ['App.Controllers']);
+angular.module('App', ['App.Controllers']);
--- smirik/static/js/controllers.js
+++ smirik/static/js/controllers.js
@@ -1,8 +1,20 @@
-var app = angular.module('App.Controllers', ['ngResource']);
-var StockController = function($scope, $http){
-    $http.post(url: '/stock-list/').
-        success(function(data, status, headers, config) {
-            $scope.stocks = data;
-        });
-}
-app.controller('StockController', StockController);
+angular.module('App.Controllers', []).
+    controller('StockCtrl', ['$scope', '$http', function($scope, $http){
+        $scope.message = "Загрузка...";
+        $http.get('/stock-list/').
+            success(function(data, status, headers, config) {
+                if (data.length > 0) {
+                    $scope.stocks = data;
+                    delete $scope.message;
+
+                    $scope.stock_fields = [];
+                    for (var prop in data[0]) {
+                        $scope.stock_fields.push(prop);
+                    }
+                } 
+                else $scope.message = "У Вас нет стоков.";
+            }).
+            error(function(data, status, headers, config) {
+                $scope.message = "Ошибка загрузки данных";
+            });
+    }]);
